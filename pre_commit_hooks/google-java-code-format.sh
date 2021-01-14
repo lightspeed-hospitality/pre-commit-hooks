20958424ae7b54dc4e598a4be2e61ac3865b14ad
Urs Keller <urs@revault.ch>
2021-01-14 19:07:26 +0100

    fix formatter, no longer formats

--- pre_commit_hooks/google-java-code-format.sh
+++ pre_commit_hooks/google-java-code-format.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 export VERSION=1.9
 mkdir -p .cache
-pushd .cache
+pushd .cache > /dev/null
 
 JAVA_VERSION=$(java -version 2>&1 | head -1 | sed -E 's/.*version "([^"]+)"/\1/g')
 if [[ ${JAVA_VERSION} == 1.8* ]]; then
@@ -13,6 +13,6 @@ then
     curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-${VERSION}/google-java-format-${VERSION}-all-deps.jar"
     chmod 755 google-java-format-${VERSION}-all-deps.jar
 fi
-popd
+popd > /dev/null
 
 java -jar .cache/google-java-format-${VERSION}-all-deps.jar --aosp --replace "$@"
