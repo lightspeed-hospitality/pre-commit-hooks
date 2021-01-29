a1c3065e652fea117554b73881a508b2a3259976
Urs Keller <urs.keller@lightspeedhq.com>
2021-01-29 14:13:00 +0100

    allow to specify formatter version (#5)

--- pre_commit_hooks/google-java-code-format.sh
+++ pre_commit_hooks/google-java-code-format.sh
@@ -1,18 +1,33 @@
-#!/usr/bin/env sh
-export VERSION=1.9
+#!/usr/bin/env bash
+FORMATTER_VERSION=1.9
+
 mkdir -p .cache
 pushd .cache > /dev/null
 
 JAVA_VERSION=$(java -version 2>&1 | head -1 | sed -E 's/.*version "([^"]+)"/\1/g')
 if [[ ${JAVA_VERSION} == 1.8* ]]; then
-    export VERSION=1.7
+    export FORMATTER_VERSION=1.7
 fi
 
-if [ ! -f google-java-format-${VERSION}-all-deps.jar ]
+formatter_args=()
+for arg in "$@" ; do
+    case "$arg" in
+	--formatter-version=*)
+	    FORMATTER_VERSION="$(echo $arg| sed 's/.*=//g')"
+	;;
+	*)
+	    formatter_args+=("$arg")
+	;;
+    esac
+done
+
+if [ ! -f google-java-format-${FORMATTER_VERSION}-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-${VERSION}/google-java-format-${VERSION}-all-deps.jar"
-    chmod 755 google-java-format-${VERSION}-all-deps.jar
+    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-${FORMATTER_VERSION}/google-java-format-${FORMATTER_VERSION}-all-deps.jar"
+    chmod 755 google-java-format-${FORMATTER_VERSION}-all-deps.jar
 fi
 popd > /dev/null
+echo "Using formatter version ${FORMATTER_VERSION}"
 
-java -jar .cache/google-java-format-${VERSION}-all-deps.jar --aosp --replace "$@"
+java -jar .cache/google-java-format-${FORMATTER_VERSION}-all-deps.jar --aosp --replace "$formatter_args"
+
