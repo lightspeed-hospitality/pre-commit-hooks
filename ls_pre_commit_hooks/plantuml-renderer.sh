fd93f106e5243014ba740beeb4dae2b3a5ee7f09
ls-rein-martha <rein.martha@lightspeedhq.com>
2022-06-10 09:37:23 +0200

    Test first.

--- ls_pre_commit_hooks/plantuml-renderer.sh
+++ ls_pre_commit_hooks/plantuml-renderer.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+if [[ -n $CIRCLECI ]]; then
+ echo "Only to be executed locally, as a pre-commit hook."
+ exit 0;
+fi
+
 if ! command -v plantuml &>/dev/null; then
   if command -v brew &>/dev/null; then
     brew install plantuml
   else
-    >&2 echo 'plantuml command not found. See https://plantuml.com/download for installation instructions.'
-    exit 1
+    >&2 echo 'plantuml cannot be installed with brew. Get plantuml from https://plantuml.com/download.'
   fi
 fi
 
 for puml_file in "${@}"; do
-  plantuml -tsvg "${@}" -o "images"
+  plantuml -tsvg "$puml_file" -o "images"
+  echo "$puml_file"
+  echo "${puml_file%.*}.svg"
+#  git add "${puml_file%.*}.svg"
 done
-git add \*.svg
+
+git status
