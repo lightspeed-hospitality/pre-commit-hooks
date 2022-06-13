c021d9054eeb852e15cf75ab4f87c0cd02797cf3
ls-rein-martha <rein.martha@lightspeedhq.com>
2022-06-13 10:49:20 +0200

    Use diff for file and add only if it is new

--- ls_pre_commit_hooks/plantuml-renderer.sh
+++ ls_pre_commit_hooks/plantuml-renderer.sh
@@ -15,16 +15,40 @@ fi
 
 changed_files=0
 for puml_file in "${@}"; do
-  plantuml -tsvg "$puml_file" -o "images"
-  FILEDIR="${puml_file%/*}/images"
   FILENAME="${puml_file##*/}"
-  SVG_FILE="$FILEDIR/${FILENAME%.*}.svg"
-  echo "" >> $SVG_FILE # add new line for tidy-xml conflict
-  # Check if the svg file is dirty in the git unstaged changes
-  if ! git diff --quiet -- "$SVG_FILE" ; then
+  ORIDIR="${puml_file%/*}"
+
+  TMPDIR="_tmp_"
+
+  plantuml -tsvg "$puml_file" -o "${TMPDIR}"
+  TMPDIR="${ORIDIR}/${TMPDIR}"
+  SVG_TMPFILE="${TMPDIR}/${FILENAME%.*}.svg"
+  echo "" >> $SVG_TMPFILE # add new line for tidy-xml conflict
+
+
+  OUTPUTDIR="${ORIDIR}/images"
+  SVG_FILE="$OUTPUTDIR/${FILENAME%.*}.svg"
+
+  # Check if existing svg file exist
+  if test -f "$SVG_FILE"; then
+    # Check if the existing svg file is identical with the generated
+    if ! git diff --quiet --no-index "$SVG_TMPFILE" "$SVG_FILE" ; then
+      mv -f "$SVG_TMPFILE" "$OUTPUTDIR"
+      # if it is not identical, mark as changed
+      changed_files=$((changed_files+1))
+    else
+      rm -f "$SVG_TMPFILE" "$OUTPUTDIR"
+    fi
+  else
+    # if no existing svg file exist then move and stage it.
+    mv -f "$SVG_TMPFILE" "$OUTPUTDIR"
+    echo "New svg gile generated, and staged. File: $SVG_FILE"
+    git add $SVG_FILE
     changed_files=$((changed_files+1))
   fi
+  rm -d $TMPDIR
 done
 
 # Exit with non-zero code if any of the svg files was changed
+echo "Total changed files: $changed_files."
 exit $changed_files
