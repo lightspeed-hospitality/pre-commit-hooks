96ce7889ab220950f9cea37ba80a45df072849b6
Abraham Yusuf <abraham.yusuf@lightspeedhq.com>
2022-10-24 16:15:35 +0200

    fix

--- ls_pre_commit_hooks/openapi-linter.sh
+++ ls_pre_commit_hooks/openapi-linter.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-set -ex
+set -e
 
-OPENAPI_LINTER_VERSION="${OPENAPI_LINTER_VERSION:-21-73a3f80}"
-OPENAPI_LINTER_IMAGE="809245501444.dkr.ecr.us-east-1.amazonaws.com/release/internal/image/openapi-linter"
+LINTER_TAG="${OPENAPI_LINTER_VERSION:-21-73a3f80}"
+LINTER_IMAGE="809245501444.dkr.ecr.us-east-1.amazonaws.com/release/internal/image/openapi-linter"
 
 container_exists() {
-  if [ $(docker ps -a -f "name=$1" | wc -l) -eq 2 ]; then
+  if [ "$(docker ps -a -f name=$1 | wc -l)" -eq 2 ]; then
     return 0
   else
     return 1
@@ -15,22 +15,22 @@ container_exists() {
 run_circle_ci() {
   linter="$1"
   docker_image="$2"
-  container="$linter-linter-volume"
+  container="${linter}-linter-volume"
 
   shift 2
 
-  if container_exists $container; then
-    docker container rm -f $container
+  if container_exists "${container}"; then
+    docker container rm -f "${container}"
   fi
 
   # create a dummy container which will hold a volume
-  docker create -v /src --name $container busybox /bin/true
+  docker create -v /src --name "${container}" busybox /bin/true
 
   # copy sources into this volume
-  docker cp ${PWD}/. $container:/src
+  docker cp "${PWD}/." "${container}:/src"
 
   # run linter
-  docker run --volumes-from $container --workdir /src $docker_image $linter "$@"
+  docker run --volumes-from "${container}" --workdir /src "${docker_image}" "${linter}" "$@"
 }
 
 run_local() {
@@ -39,20 +39,20 @@ run_local() {
 
   shift 2
 
-  docker run -v $PWD:/src:rw,Z --workdir /src $docker_image $linter "$@"
+  docker run -v "${PWD}:/src:rw,Z" --workdir /src "${docker_image}" "${linter}" "$@"
 }
 
 run() {
   linter="$1"
-  docker_image="$OPENAPI_LINTER_IMAGE:$OPENAPI_LINTER_VERSION"
+  docker_image="${LINTER_IMAGE}:${LINTER_TAG}"
 
   shift
 
-  if [[ -n $CIRCLECI ]]; then
+  if [[ -n "${CIRCLECI}" ]]; then
     echo "Running ${linter} with CircleCI docker volume workaround."
-    run_circle_ci $linter $docker_image "$@"
+    run_circle_ci "${linter}" "${docker_image}" "$@"
   else
-    run_local $linter $docker_image "$@"
+    run_local "${linter}" "${docker_image}" "$@"
   fi
 }
 
